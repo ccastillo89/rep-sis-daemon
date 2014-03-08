e4eb2f1a494aa14548282153bb161415d906c7f0
sdramos0303@gmail.com <sdramos0303@gmail.com@ad490498-bca0-6924-05fe-5e5bf8d5f06d>
2014-03-08 02:40:02 +0000

    Usuario Permitido

---
+++ trunk/sistemaUpc/src/upc/modelo/UsuarioPermitido.java
@@ -0,0 +1,26 @@
+package upc.modelo;
+
+public class UsuarioPermitido {
+	private Comentario Comentario;
+	private Usuario usuario;
+	private int puntos;
+	public Comentario getComentario() {
+		return Comentario;
+	}
+	public void setComentario(Comentario comentario) {
+		Comentario = comentario;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	public int getPuntos() {
+		return puntos;
+	}
+	public void setPuntos(int puntos) {
+		this.puntos = puntos;
+	}
+
+}
