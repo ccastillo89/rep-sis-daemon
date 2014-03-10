e548edc91f7b582dc68a955d6b57c8607dfa8825
jivansf@gmail.com <jivansf@gmail.com@ad490498-bca0-6924-05fe-5e5bf8d5f06d>
2014-03-10 02:32:21 +0000

    Insertar publicar y actualizar

--- trunk/sistemaUpc/src/upc/negocio/GestionPublicaciones.java
+++ trunk/sistemaUpc/src/upc/negocio/GestionPublicaciones.java
@@ -1,40 +1,59 @@
 package upc.negocio;
 
+import java.sql.Date;
 import java.util.Collection;
-
 import upc.dao.PublicacionDAO;
 import upc.excepcion.DAOExcepcion;
 import upc.modelo.Publicacion;
 
 public class GestionPublicaciones {
 
-	/*
-	 * public Collection<Categoria> buscarPorNombre(String nombre) throws
-	 * DAOExcepcion { CategoriaDAO dao = new CategoriaDAO(); return
-	 * dao.listar(); }
-	 * 
-	 * public Categoria insertar(String nombre, String descripcion) throws
-	 * DAOExcepcion { CategoriaDAO dao = new CategoriaDAO();
-	 * 
-	 * Categoria vo = new Categoria(); vo.setNombre(nombre);
-	 * vo.setDescripcion(descripcion);
-	 * 
-	 * return dao.insertar(vo); }
-	 * 
-	 * public Categoria obtener(int idCategoria) throws DAOExcepcion {
-	 * CategoriaDAO dao = new CategoriaDAO(); return dao.obtener(idCategoria); }
-	 * 
-	 * public void eliminar(int idCategoria) throws DAOExcepcion { CategoriaDAO
-	 * dao = new CategoriaDAO(); dao.eliminar(idCategoria); }
-	 * 
-	 * public Categoria actualizar(int idCategoria, String nombre, String
-	 * descripcion) throws DAOExcepcion { CategoriaDAO dao = new CategoriaDAO();
-	 * 
-	 * Categoria vo = new Categoria(); vo.setIdCategoria(idCategoria);
-	 * vo.setNombre(nombre); vo.setDescripcion(descripcion);
-	 * 
-	 * return dao.actualizar(vo); }
-	 */ 
+	public Publicacion insertar(String titulo, String descripcion,String archivo,int idUsuario,
+				int estado,Date fechaCreacion,String palabraClave)
+			throws DAOExcepcion {
+		
+		PublicacionDAO dao = new PublicacionDAO();
+		Publicacion vo = new Publicacion();
+		vo.setTitulo(titulo);
+		vo.setDescripcion(descripcion);
+		vo.setArchivo(archivo);
+		vo.setIdUsuario(idUsuario);
+		vo.setEstado(estado);
+		vo.setFechaCreacion(fechaCreacion);
+		vo.setPalabraClave(palabraClave);
+				
+		return dao.insertar(vo);
+	}
+	
+	public Publicacion actualizar(int idPublicacion,String titulo, String descripcion,String archivo,String palabraClave)
+					throws DAOExcepcion {
+		
+		PublicacionDAO dao = new PublicacionDAO();
+		Publicacion vo = new Publicacion();
+		
+		vo.setIdPublicacion(idPublicacion);
+		vo.setTitulo(titulo);
+		vo.setDescripcion(descripcion);
+		vo.setArchivo(archivo);
+		vo.setPalabraClave(palabraClave);
+		
+		return dao.actualizar(vo);
+	}
+	
+	
+	public Publicacion publicar(int idPublicacion,int estado,Date fechaPublicacion)
+							throws DAOExcepcion {
+				
+				PublicacionDAO dao = new PublicacionDAO();
+				Publicacion vo = new Publicacion();
+				
+				vo.setIdPublicacion(idPublicacion);
+				vo.setEstado(estado);
+				vo.setFechaPublicacion(fechaPublicacion);
+												
+				return dao.actualizar(vo);
+			}
+	
 	  public Collection<Publicacion> ReportedePublicaciones() throws DAOExcepcion { PublicacionDAO
 	  dao = new PublicacionDAO(); return dao.ReportedePublicaciones(); }
 	 
