02f8ac79b5ae365db4d058f1b6824017c4f1a945
jivansf <jivansf@gmail.com>
2014-03-12 08:22:43 +0000

    utilitario fechas

--- trunk/sistemaUpc/src/upc/util/utilfechas.java
+++ trunk/sistemaUpc/src/upc/util/utilfechas.java
@@ -1,7 +1,15 @@
 package upc.util;
-
+import java.util.Calendar;
+import java.sql.Date;
 public class utilfechas {
 	
-	
+	public static Date getFecha(int dia, int mes, int anio) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.DAY_OF_MONTH,dia);
+        cal.set(Calendar.MONTH,mes);
+        cal.set(Calendar.YEAR,anio);
+        Date d = (Date) cal.getTime();
+        return d;        
+    }  
 
 }
