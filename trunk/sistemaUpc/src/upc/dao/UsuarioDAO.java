90d2e42a090bae8a4bd14a1786b8508458c73931
carlosdevelop <carlosdevelop@gmail.com>
2014-03-13 03:11:13 +0000

    Utilizacion de la clase codigos

---
+++ trunk/sistemaUpc/src/upc/dao/UsuarioDAO.java
@@ -0,0 +1,16 @@
+package upc.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import upc.excepcion.DAOExcepcion;
+import upc.modelo.Usuario;
+import upc.util.ConexionBD;
+
+public class UsuarioDAO {
+
+}
